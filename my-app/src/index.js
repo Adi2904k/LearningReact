2ded956077e1cc9bb01527a04f4897672e9f5150
Adi2904k <adityaamalusare14@gmail.com>
2024-03-27 11:53:08 +0530

    React.Fragment

---
+++ my-app/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM  from 'react-dom';
+
+ReactDOM.render(
+  <React.Fragment >
+    <h1>I am learning ReactJs</h1>
+    <h2>Hello Now</h2>
+   
+  </React.Fragment>,
+  document.getElementById("root"),
+  ()=>console.log("rendered")
+);
